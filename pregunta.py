c2c1df913bdc21756b5db3853ba41727274aba21
ssuarezp <125924812+ssuarezp@users.noreply.github.com>
2023-09-28 15:45:43 -0500

    20230928

--- pregunta.py
+++ pregunta.py
@@ -17,5 +17,18 @@ def ingest_data():
     #
     # Inserte su código aquí
     #
+  df1 = pd.read_fwf("clusters_report.txt", skiprows=4, names=['1', '2', '3', '4'])
+  df = pd.DataFrame(columns=['cluster', 'cantidad_de_palabras_clave', 'porcentaje_de_palabras_clave', 'principales_palabras_clave'])
+  for ind in range(len(df1)):
+    if not (np.isnan(df1.iloc[ind]['1'])):
+      df.loc[len(df.index)] = [df1.iloc[ind]['1'], df1.iloc[ind]['2'], df1.iloc[ind]['3'], df1.iloc[ind]['4']]
+    else:
+      df.at[len(df.index)-1, 'principales_palabras_clave'] += " " + df1.iloc[ind]['4']
+  df['principales_palabras_clave'] = df['principales_palabras_clave'].apply(lambda x: re.sub('\s+', ' ', x))
+  df['principales_palabras_clave'] = df['principales_palabras_clave'].apply(lambda x: re.sub(',\s*', ', ', x))
+  df['principales_palabras_clave'] = df['principales_palabras_clave'].apply(lambda x: x.rstrip('.'))
+  df['porcentaje_de_palabras_clave'] = df['porcentaje_de_palabras_clave'].apply(lambda x: float(re.sub(',', '.', re.findall('\d+,\d+', x)[0])))
+  df['cluster'] = df['cluster'].apply(int)
+  df['cantidad_de_palabras_clave'] = df['cantidad_de_palabras_clave'].apply(int)
 
-    return df
+  return df
